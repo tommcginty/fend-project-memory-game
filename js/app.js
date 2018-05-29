e6ebf78ff409c117b44f7c5ef820fbd20ac9a462
Tom McGinty <thmcginty@gmail.com>
2018-05-29 19:18:44 -0400

    Feat: Added event listener to cards

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const deck = document.querySelector('.deck');
+
 /*
  * Create a list that holds all of your cards
  */
@@ -24,15 +26,31 @@ function shuffle(array) {
 
     return array;
 }
+function cardClicked(evt) {
+  if (evt.target.nodeName === 'LI') // ← verifies target is desired element
+    console.log('I was clicked: ' + evt.target);
+};
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+//set up the event listener for a card. If a card is clicked (cardClicked):
+deck.addEventListener('click', cardClicked);
+
+
+
+// - display the card's symbol (put this functionality in another function that you call from cardClicked)
+
+
+//  - add the card to a *list* of "open" cards (put this functionality in another function that you call from cardClicked)
+//  - if the list already has another card, check to see if the two cards match
+
+
+//   + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from cardClicked)
+
+
+//   + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+
+
+//   + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+
+
+//   + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
