835e8411f6270c3df07b9d37dcd526fcc2e34362
Tom McGinty <thmcginty@gmail.com>
2018-06-03 14:56:36 -0400

    feat ad function to count Moves

--- js/app.js
+++ js/app.js
@@ -48,7 +48,7 @@ function shuffle(array) {
 
 // - display the card's symbol (put this functionality in another function that you call from cardClicked)
 function displaySymbol(card){
-  card.classList.add('show');
+  card.classList.add('open', 'show');
 }
 
 //  - add the card to a *list* of "open" cards (put this functionality in another function that you call from cardClicked)
@@ -65,33 +65,53 @@ function checkMatch() {
 }
 
 //   + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from cardClicked)
-
+function matchCards(){
+  for (let i = 0; i < 2; i++)
+    tab[cardList[i]].classList.add('match');
+}
 
 //   + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
 function flipCards() {
+  setTimeout(function(){
   for (let i = 0; i < 2; i++){
-    tab[cardList[0]].classList.remove('show')
+    tab[cardList[0]].classList.remove('show', 'open');
     cardList.shift();
-  }
-
-
+    };
+  }, 1000);
 }
 
 //   + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-
+function addToCounter(counter){
+  let moves = counter/2;
+  if (moves === 1)
+    document.getElementById('moves').textContent = moves + ' Move';
+  else
+    document.getElementById('moves').textContent = moves + ' Moves';
+}
 
 //   + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+function gameOver(){
+  //create a div over the 
+}
 
-function cardClicked(evt) {
+
+function cardClicked() {
   if (items[index].className != 'card show'){
     counter++;
     let cardFace = items[index].querySelector('i').className;
     console.log('I was clicked: ' + index);
     displaySymbol(items[index]);
     addToCardList(index);
-    if (counter % 2 == 0)
+    if (counter % 2 == 0){
       if (!checkMatch())
         flipCards();
+      else
+        matchCards();
+
+      addToCounter(counter);
+      };
+    if (cardList.length == 16)
+      gameOver();
   };
 }
 
