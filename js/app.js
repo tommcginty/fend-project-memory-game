4618a0b3b95ba4fe82ecf8b2de69646181170193
Tom McGinty <thmcginty@gmail.com>
2018-06-03 12:07:49 -0400

    feat: Add function to flip cards back if they do not match

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 const deck = document.querySelector('.deck');
 let cardList = [];
 let counter = 0;
+let items = document.querySelectorAll(".deck li"),
+    tab = [], index;
 
+// add values to the array
+for(let i = 0; i < items.length; i++){
+tab.push(items[i]);
+}
+
+// get selected element index
+for(var i = 0; i < items.length; i++)
+{
+items[i].onclick = function(){
+
+   index = tab.indexOf(this);
+   return index;
+};
+}
 /*
  * Create a list that holds all of your cards
  */
@@ -36,24 +52,30 @@ function displaySymbol(card){
 }
 
 //  - add the card to a *list* of "open" cards (put this functionality in another function that you call from cardClicked)
-function addToList(card) {
-  cardList.push(card);
+function addToCardList(cardIndex) {
+  cardList.unshift(cardIndex);
   }
 
   //  - if the list already has another card, check to see if the two cards match
 function checkMatch() {
-  if (cardList[cardList.length - 1]== cardList[cardList.length - 2])
+  if (tab[cardList[0]].innerHTML == tab[cardList[1]].innerHTML)
   	return true;
   else
   	return false;
 }
 
-
 //   + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from cardClicked)
 
 
 //   + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+function flipCards() {
+  for (let i = 0; i < 2; i++){
+    tab[cardList[0]].classList.remove('show')
+    cardList.shift();
+  }
 
+
+}
 
 //   + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
 
@@ -61,14 +83,16 @@ function checkMatch() {
 //   + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
 
 function cardClicked(evt) {
-  if (evt.target.nodeName === 'LI' && evt.target.className != 'show'){
-    let cardFace = evt.target.querySelector('i').className;
-    console.log('I was clicked: ' + cardFace);
-    displaySymbol(evt.target);
-    addToList(cardFace);
-    if (counter % 2 == 0 && counter > 1)
-      checkMatch();
-  }
+  if (items[index].className != 'card show'){
+    counter++;
+    let cardFace = items[index].querySelector('i').className;
+    console.log('I was clicked: ' + index);
+    displaySymbol(items[index]);
+    addToCardList(index);
+    if (counter % 2 == 0)
+      if (!checkMatch())
+        flipCards();
+  };
 }
 
 
